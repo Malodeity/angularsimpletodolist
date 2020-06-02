3bc60cc5afe5518f223240b20ceb64af9954e6f0
Malo <malomthethwa@outlook.com>
2020-06-02 15:51:40 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import {Todo} from "./todo";
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //for injecting a code to a certain html file
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crashcourse';
+  title = 'Project CC';
+
+
+  todoValue:string;
+  list: Todo[];
+
+
+
+
+  ngOnInit(){
+
+   this.list = [];
+   this.todoValue = "";
+  
+    }
+
+    AddItem(){
+
+      if(this.todoValue !== ""){
+        const newItem : Todo = {
+          id: Date.now(),
+          value: this.todoValue,
+          isDone: false
+        };
+
+          this.list.push(newItem);
+      }
+
+      this.todoValue = "";
+    }
+    
+    deleteItem(id:number){
+
+       this.list = this.list.filter(item => item.id !== id);
+
+    }
+
+
 }
